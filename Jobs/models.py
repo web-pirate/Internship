74944e77ce06928065a228b12470c71c74bc1e75
zoldyck25 <93246149+zoldyck25@users.noreply.github.com>
2022-07-10 18:54:26 +0530

    changes

--- Jobs/models.py
+++ Jobs/models.py
@@ -1,3 +1,26 @@
+from email.utils import localtime
+import uuid
 from django.db import models
-
+from pkg_resources import Requirement
+job_type={
+        ("full-time","full-time"),
+        ("part-time","part-time"),
+    }
 # Create your models here.
+class job_profile(models.Model):
+    id=models.UUIDField(primary_key=True,editable=False,default=uuid.uuid4)
+    jobtitle=models.CharField(max_length=50)
+    jobcategory=models.CharField(max_length=50)
+    companyname=models.CharField(max_length=50)
+    companymail=models.EmailField(max_length=50)
+    location=models.CharField(max_length=50)
+    jobtype=models.CharField(max_length=20,choices=job_type,default=None,null=True)
+    jobtags=models.CharField(max_length=50)
+    salary=models.IntegerField()
+    experience=models.CharField(max_length=50)
+    aboutcompany=models.TextField()
+    responsibilities=models.TextField()
+    Requirements=models.TextField()
+    benefits=models.TextField()
+    jobdescription=models.TextField()
+     
--- Jobs/urls.py
+++ Jobs/urls.py
@@ -4,8 +4,8 @@ from django.urls.conf import include
 from . import views
 
 urlpatterns = [
-    path("job", views.job, name="job"),
-    path("job-details", views.job_details, name="job-details"),
+    path("job-posted/<companyname>", views.job_posted, name="job-posted"),
+    path("job-details/<companyname>/<id>/", views.job_details, name="job-details"),
     path("job-posts", views.job_posts, name="job-posts"),
     
     
